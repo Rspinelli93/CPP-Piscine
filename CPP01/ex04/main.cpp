1b5e76ceafe74ff7e2a99fc06ee0cd65b1b5c51b
Rick <rspinellir13@gmail.com>
2026-05-08 11:42:23 +0200

    missing loop

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
+#include <fstream> 
 #include <string>
 
 int main(int ac, char *av[])
 {
-	if (ac == 3)
+	if (ac == 4)
 	{
+		//+ Open the file
+		std::ifstream ogFile(av[1]);
+		if (!ogFile.is_open())
+		{
+			std::cout << "Err: file not found" << std::endl;
+			return (1);
+		}
+
+		//+ Create and open a text file
+		std::string fileName;
+		fileName.assign(av[1]);
+		fileName.append(".replace");
+		std::ofstream outputFile(fileName.c_str());
+		std::string Buff;
+
+		//+ Populate the output file
+		while (getline (ogFile, Buff))
+		{
+			outputFile << Buff;
+			outputFile << "\n";
+		}
+
+		//+ Close the files and exit
+		outputFile.close(); 
+		ogFile.close();
 		return (0);
 	}
 	std::cout << "Expected 2 agruments" << std::endl;
-	std::cout << "Usage: ./sed_is_for_losers [word] [replacement]" << std::endl;
+	std::cout << "Usage: ./sed [word] [replacement]" << std::endl;
 	return (1);
 }
+
+//*NOTE: NOW GO INSIDE THE WHILE LOOP AND DO THIS:
+/* 
+ac == 4 check (filename, s1, s2)
+s1.empty() check
+
+ifstream ogFile(av[1])
+ofstream newFile(av[1] + ".replace")
+is_open() check both
+
+while getline(ogFile, line):
+    pos = line.find(s1)
+    while pos != string::npos:
+        line.erase(pos, s1.length())
+        line.insert(pos, s2)
+        pos = line.find(s1, pos + s2.length())
+    newFile << line << "\n"
+
+ogFile.close()
+newFile.close() */
